955f433d6e91f2488a3071d610da81a98ab320e1
Rizwan Shaikh <devilt761@gmail.com>
2024-01-31 00:29:57 +0530

    I

---
+++ _plugins/unzip.rb
@@ -0,0 +1,21 @@
+require 'zip'
+
+def unzip_file(zip_file, destination)
+  # Create the destination directory if it doesn't exist
+  Dir.mkdir(destination) unless Dir.exist?(destination)
+
+  Zip::File.open(zip_file) do |zip|
+    zip.each do |entry|
+      entry.extract(File.join(destination, entry.name))
+    end
+  end
+end
+
+zip_files = ['img-1.zip', 'img-2.zip']
+destination = 'images'
+
+zip_files.each do |zip_file|
+  unzip_file(zip_file, destination)
+end
+
+puts 'Success'
