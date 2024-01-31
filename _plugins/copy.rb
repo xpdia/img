7a3b5629724c497ffc2387e27281937976c94e1b
imrznx <147604854+imrznx@users.noreply.github.com>
2024-01-31 21:06:54 +0530

    I

---
+++ _plugins/copy.rb
@@ -0,0 +1,16 @@
+require 'fileutils'
+
+image_path = 'main.webp' # Replace with the actual path to the main.webp image
+webp_directory = '0' # Replace with the actual path to the WebP directory
+
+# Create the WebP directory if it doesn't exist
+FileUtils.mkdir_p(webp_directory)
+
+image_data = File.binread(image_path)
+
+(1..5).each do |index|
+  webp_file = File.join(webp_directory, "#{index}.webp")
+  File.binwrite(webp_file, image_data)
+  
+  puts "#{image_path} copied as #{webp_file}"
+end
